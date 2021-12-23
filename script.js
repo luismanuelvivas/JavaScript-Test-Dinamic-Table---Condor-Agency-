8e522df1e3763470e5516765ce3d65254dc156a0
Luis Manuel Vivas <luis.manuel.vivas.r@gmail.com>
2021-12-23 11:09:00 -0400

    Delete, edit and search function. Ready

--- script.js
+++ script.js
@@ -7,8 +7,36 @@ let deleteMode = false;
 let editBtn = document.getElementById('editBtn')
 let editMode = false;
 let searchBtn = document.getElementById('searchBtn')
-let inputSearchBar = document.getElementById('input-search-bar')
+let inputSearchBar = document.getElementById('inputSearchBar')
 let showAllBtn = document.getElementById('showAllBtn')
+
+deleteBtn.addEventListener('click', function(){
+    
+    if(deleteMode === false){
+        deleteMode = true;
+        editMode =false; 
+        itemsContainer.style.color = 'red'
+        checkedItemsContainer.style.color ='red'
+    }else{
+        deleteMode = false
+        itemsContainer.style.color = 'black'
+        checkedItemsContainer.style.color ='black'
+    }
+
+})
+
+editBtn.addEventListener('click', function(){
+    
+    if(editMode === false){
+        editMode = true;
+        deleteMode = false;
+        itemsContainer.style.color = 'blue'
+    }else{
+        editMode = false
+        itemsContainer.style.color = 'black'
+    }
+
+})
 
 addItemBtn.addEventListener('click',function(){
     var paragraph = document.createElement('p');
@@ -17,21 +45,64 @@ addItemBtn.addEventListener('click',function(){
     itemsContainer.appendChild(paragraph);
     inputField.value = "";
 
+    searchBtn.addEventListener('click', function(){
+        if(inputSearchBar.value != ''){
+            if(paragraph.innerText.includes(inputSearchBar.value) === false){  
+                paragraph.style.display = 'none';
+            }
+        }else{
+            window.alert('The input is empty')
+        }     
+    })
+
+    showAllBtn.addEventListener('click', function(){ 
+        paragraph.style.display = 'block';
+        inputSearchBar.value = '';
+    })
+
     paragraph.addEventListener('click', function(){
 
+        if(deleteMode === true){
+            itemsContainer.removeChild(paragraph);
+        }else if(editMode === true){
+            if(inputField.value != ''){
+                paragraph.innerText = inputField.value;
+                inputField.value = '';
+            }else{
+                window.alert('The input is empty!')
+            }
+        }else if(deleteMode ===false && editMode === false){
             var paragraph2 = document.createElement('p');
             paragraph2.innerText = paragraph.innerText; 
             checkedItemsContainer.appendChild(paragraph2);
             itemsContainer.removeChild(paragraph);
-        
+        }
 
+        searchBtn.addEventListener('click',function(){
+
+            if(inputSearchBar.value != ''){
+                if(paragraph2.innerText.includes(inputSearchBar.value)== false){  
+                    paragraph2.style.display = 'none';
+                }
+            }else{
+                window.alert('The input is empty')
+            }
+
+        })
+
+        showAllBtn.addEventListener('click', function(){ 
+            paragraph2.style.display = 'block';
+            inputSearchBar.value = '';
+        })
 
         paragraph2.addEventListener('click', function(){
-    
-            checkedItemsContainer.removeChild(paragraph2);
-            paragraph.innerText = paragraph2.innerText;
-            itemsContainer.appendChild(paragraph);
-            
+            if(deleteMode === true){
+                checkedItemsContainer.removeChild(paragraph2);
+            }else{
+                checkedItemsContainer.removeChild(paragraph2);
+                paragraph.innerText = paragraph2.innerText;
+                itemsContainer.appendChild(paragraph);
+            }
         })
     })    
 })
