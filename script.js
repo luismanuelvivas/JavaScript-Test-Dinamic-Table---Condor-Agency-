9fe974cadeacf7d8dd6d1f16b7cc7b39eceb0aab
Luis Manuel Vivas <luis.manuel.vivas.r@gmail.com>
2021-12-23 13:51:55 -0400

    Adding conditional to avoid blank paragraphs

--- script.js
+++ script.js
@@ -52,72 +52,75 @@ editBtn.addEventListener('click', function(){
 })
 
 addItemBtn.addEventListener('click',function(){
-    var paragraph = document.createElement('p');
-    paragraph.classList.add('paragraph-styling');
-    paragraph.innerText = inputField.value; 
-    itemsContainer.appendChild(paragraph);
-    inputField.value = "";
-
-    searchBtn.addEventListener('click', function(){
-        if(inputSearchBar.value != ''){
-            if(paragraph.innerText.includes(inputSearchBar.value) === false){  
-                paragraph.style.display = 'none';
-            }
-        }else{
-            window.alert('The input is empty')
-        }     
-    })
-
-    showAllBtn.addEventListener('click', function(){ 
-        paragraph.style.display = 'block';
-        inputSearchBar.value = '';
-    })
-
-    paragraph.addEventListener('click', function(){
-
-        if(deleteMode === true){
-            itemsContainer.removeChild(paragraph);
-        }else if(editMode === true){
-            if(inputField.value != ''){
-                paragraph.innerText = inputField.value;
-                inputField.value = '';
-                section1.className = 'section1';
-                editMode =  false;
-            }else{
-                window.alert('The input is empty!')
-            }
-        }else if(deleteMode ===false && editMode === false){
-            var paragraph2 = document.createElement('p');
-            paragraph2.innerText = paragraph.innerText; 
-            checkedItemsContainer.appendChild(paragraph2);
-            itemsContainer.removeChild(paragraph);
-        }
-
-        searchBtn.addEventListener('click',function(){
-
+    if(inputField.value != ''){
+        var paragraph = document.createElement('p');
+        paragraph.classList.add('paragraph-styling');
+        paragraph.innerText = inputField.value; 
+        itemsContainer.appendChild(paragraph);
+        inputField.value = "";
+    
+        searchBtn.addEventListener('click', function(){
             if(inputSearchBar.value != ''){
-                if(paragraph2.innerText.includes(inputSearchBar.value)== false){  
-                    paragraph2.style.display = 'none';
+                if(paragraph.innerText.includes(inputSearchBar.value) === false){  
+                    paragraph.style.display = 'none';
                 }
             }else{
                 window.alert('The input is empty')
-            }
-
+            }     
         })
-
+    
         showAllBtn.addEventListener('click', function(){ 
-            paragraph2.style.display = 'block';
+            paragraph.style.display = 'block';
             inputSearchBar.value = '';
         })
-
-        paragraph2.addEventListener('click', function(){
+    
+        paragraph.addEventListener('click', function(){
+    
             if(deleteMode === true){
-                checkedItemsContainer.removeChild(paragraph2);
-            }else{
-                checkedItemsContainer.removeChild(paragraph2);
-                paragraph.innerText = paragraph2.innerText;
-                itemsContainer.appendChild(paragraph);
+                itemsContainer.removeChild(paragraph);
+            }else if(editMode === true){
+                if(inputField.value != ''){
+                    paragraph.innerText = inputField.value;
+                    inputField.value = '';
+                    section1.className = 'section1';
+                    editMode =  false;
+                }else{
+                    window.alert('The input is empty!')
+                }
+            }else if(deleteMode ===false && editMode === false){
+                var paragraph2 = document.createElement('p');
+                paragraph2.innerText = paragraph.innerText; 
+                checkedItemsContainer.appendChild(paragraph2);
+                itemsContainer.removeChild(paragraph);
             }
-        })
-    })    
+    
+            searchBtn.addEventListener('click',function(){
+    
+                if(inputSearchBar.value != ''){
+                    if(paragraph2.innerText.includes(inputSearchBar.value)== false){  
+                        paragraph2.style.display = 'none';
+                    }
+                }else{
+                    window.alert('The input is empty')
+                }
+    
+            })
+    
+            showAllBtn.addEventListener('click', function(){ 
+                paragraph2.style.display = 'block';
+                inputSearchBar.value = '';
+            })
+    
+            paragraph2.addEventListener('click', function(){
+                if(deleteMode === true){
+                    checkedItemsContainer.removeChild(paragraph2);
+                }else{
+                    checkedItemsContainer.removeChild(paragraph2);
+                    paragraph.innerText = paragraph2.innerText;
+                    itemsContainer.appendChild(paragraph);
+                }
+            })
+        })    
+    }
+    
 })
